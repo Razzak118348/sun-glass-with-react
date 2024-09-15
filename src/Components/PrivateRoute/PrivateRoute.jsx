f28defae11c7151dd0cab5589e379be8b6795f16
razzak118348 <abdurrazzak118348@gmail.com>
2024-09-15 16:12:29 +0600

    loading spinner

--- src/Components/PrivateRoute/PrivateRoute.jsx
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+
 import useAuth from '../../hooks/useAuth';
 import { Navigate, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({children}) => {
     const { user ,loading} = useAuth();
     const location = useLocation();
 
 
     if (loading) {
-        return <h1 className="text-4xl">Loading</h1>
+        return <h1 className="text-4xl text-center"><span className="loading loading-ring loading-lg"></span> </h1>
     }
 
     if (!user) {
@@ -20,5 +21,9 @@ const PrivateRoute = ({ children }) => {
         </div>
     );
 };
+
+PrivateRoute.propTypes={
+children:PropTypes.node,
+}
 
 export default PrivateRoute;
